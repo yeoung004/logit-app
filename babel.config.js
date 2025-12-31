5f889b1dc1b0e458ad26748142645fa29d80c420
최영성 <dudtjd4149@gmail.com>
2025-12-30 21:05:54 -0500

    add: custom font, Unistyles, and update app config
    Integrated PretendardVariable custom font and react-native-unistyles for theming. Updated app.json with new slug, bundle/package identifiers, and added react-native-edge-to-edge plugin. Added babel config for Unistyles, initialized unistyles.ts with theme settings, and updated _layout.tsx to load fonts and apply the theme. Also added new clean scripts and dependencies in package.json.

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = function (api) {
+  api.cache(true)
+
+  return {
+    presets: ['babel-preset-expo'],
+
+    plugins: [
+        ['react-native-unistyles/plugin', {
+            root: 'src'
+        }]
+    ]
+  }
+}
